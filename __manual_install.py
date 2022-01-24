2236b003fd5ad784d1400623875a01dd038cda4a
xeelix <fritlangoof@gmail.com>
2022-01-24 16:59:39 +0700

    Added path join
    Former-commit-id: 77034ecc1c1010e3a857c9308b8fa5afb1648d23

--- __manual_install.py
+++ __manual_install.py
@@ -6,7 +6,6 @@ def install(name):
 
 my_packages = ['requests', 'clint', 'faker', 'selenium', 'beautifulsoup4', "lxml", "colorama", "PyInstaller"]
 
-
 for package in my_packages:
     install(package)
     print('\n')
